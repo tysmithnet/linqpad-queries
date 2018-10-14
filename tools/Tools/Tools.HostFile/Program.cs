05b3976b2d88289b8e9c0194f48c3c21ee1da5d1
Ty Smith <dts5004@gmail.com>
2018-10-13 23:13:21 -0700

    Added main project to collect the binaries and a hosts project

---
+++ tools/Tools/Tools.HostFile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Tools.HostFile
+{
+    public class Options
+    {
+        [Option('w', "www", HelpText = "Include www. subdomain automatically", Default = true)]
+        public bool IncludeWwwSubdomain { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
